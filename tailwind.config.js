160f95e4c1534cfa94903238ae22a2fcd64f07c9
minhaj <minhajurr019@gmail.com>
2025-02-04 00:03:37 +0600

    complete the project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ['Poppin', 'serif'],
+      },
+    },
+  },
+  // ...
+}
